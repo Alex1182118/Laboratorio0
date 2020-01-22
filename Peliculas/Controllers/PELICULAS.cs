387f6d86eccdcda7442e3b4c33d6a100e7725026
Alex1182118 <alex.gabrielvillatoro@gmail.com>
2020-01-22 11:25:15 -0600

    Creacion de Firmas

---
+++ Peliculas/Controllers/PELICULAS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Peliculas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PELICULAS : ControllerBase
+    {
+              
+                
+        //[HttpGet]
+        //public IEnumerable<Pelicula> AñadirPelicula()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new Pelicula
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+        Stack<Pelicula> Pila = new Stack<Pelicula>();
+
+        [HttpPost]
+        public IEnumerable<Pelicula> ObtenerPeliculas()
+        {
+            
+
+            
+            
+        }
+
+
+    }
+}
---
+++ Peliculas/Pelicula.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Peliculas
+{
+    public class Pelicula
+    {
+        public string Nombre { get; set; }
+
+        public string Director { get; set; }
+
+        public int Año { get; set; }
+    }
+}
